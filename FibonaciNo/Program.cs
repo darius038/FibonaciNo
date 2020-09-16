3b832660a8942b828ad6145f7b9758898c0df8a4
Darius <darius038@gmail.com>
2020-09-16 23:47:34 +0300

    First commit

---
+++ FibonaciNo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FibonaciNo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter integer number");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Is number Fibonaci ? "+FibonaciCheck(number));
+        }
+
+        static bool FibonaciCheck(int no)
+        {
+            int i = 0;
+            int k = 1;
+            int fb;
+            for(fb =0; fb<=no;)
+            {
+                fb = i + k;
+                i = k;
+                k = fb;
+                if (fb == no)
+                {
+                    return true;
+                }
+            }            
+            return false;
+
+        }
+    }
+}
